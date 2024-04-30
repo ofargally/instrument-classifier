46a2b5570815f11bf44272a33159e8eb877309f9
atauber27 <atauber27@gmail.com>
2024-04-30 16:37:46 -0400

    CNN v1

--- nn_pipelines/cnn_pipeline.py
+++ nn_pipelines/cnn_pipeline.py
@@ -24,7 +24,6 @@ for file in csv_files_train:
     df = pd.read_csv(file)
     dataframes_train.append(df)
 pd_train = pd.concat(dataframes_train, ignore_index=True)
-print(csv_files_train)
 
 
 
@@ -34,9 +33,11 @@ def process_labels(value):
     return [int(v) for v in value.split(';')]
     
 labels = pd_train['Instruments'].apply(process_labels)
+print(labels[:30])
 
 mlb = MultiLabelBinarizer()
 labels_encoded = mlb.fit_transform(labels)
+print(labels_encoded[:30])
 
 # Prepare features
 features = pd_train.drop('Instruments', axis=1)
@@ -129,5 +130,5 @@ for epoch in range(num_epochs):
     val_loss = validate_epoch(model, val_loader, criterion, device)
     print(f'Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
 
-torch.save(model.state_dict(), './cnn_v1_state.pth')
-torch.save(model, './cnn_v1.pth')
+torch.save(model.state_dict(), './cnn_v2_state.pth')
+torch.save(model, './cnn_v2.pth')
